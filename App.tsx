44387be907e24873df0b138b711a46ebd42050c5
Hafizhan Azka <hafizhanazka92@gmail.com>
2026-02-25 09:35:11 +0700

    Initial commit - Expo TypeScript project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={require('./assets/Mao.jpeg')} style={styles.image} />
+      <Text>Halo, saya Hafizhan Azka - 2405102061</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f8ff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 432,
+    height: 324,
+    marginBottom: 20,
   },
 });
